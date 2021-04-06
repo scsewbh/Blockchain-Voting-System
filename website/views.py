7084a3c6212ed81c0d0c1a8773218545a0904260
Sanjay S <scsewbh@gmail.com>
2021-04-05 22:34:03 -0400

    Voting Enabled

--- website/views.py
+++ website/views.py
@@ -13,29 +13,35 @@ client = datastore.Client()
 @views.route('/', methods = ['GET', 'POST'])
 def vote():
     if request.method == 'POST':
-        key = client.key('UserCourses', session['userid'])
+        key = client.key('voted')
         entity = datastore.Entity(key=key)
         entity.update({
-            'MyClassList': request.form["mylistdata"]
+            'GID': session['userid']
         })
         client.put(entity)
-        print(request.form["mylistdata"])
+        print(session['userid'])
+        #ANOTHER PUSH TO MEME POOL WITH DATA
         return redirect(url_for('views.results'))
     else:
         print(session)
         if 'authenticated' in session and session['authenticated']:
             query = client.query(kind="voted")
+            query.add_filter("GID", "=", session['userid'])
             results = list(query.fetch())
-            print(results)
-            return render_template('register.html', userid=session['userid'], fname=session['fname'], lname=session['lname'], email=session['email'])
+            if not results:
+                return render_template('register.html', userid=session['userid'], fname=session['fname'],
+                                       lname=session['lname'], email=session['email'])
+            else:
+                return redirect(url_for('views.results')) #ERROR or SIGN IN FIRST
+
         else:
-            return redirect(url_for('auth.login')) #ERROR or SIGN IN FIRST
+            return redirect(url_for('auth.login'))
 
 
 @views.route('/results')
 def results():
     print(session)
-    if 'authenticated' in session and session['authenticated']:
+    if 'authenticated' in session and session['authenticated']: #and voted then render
         return render_template('voted.html', userid=session['userid'])
     else:
         return redirect(url_for('auth.login')) #ERROR or SIGN IN FIRST
